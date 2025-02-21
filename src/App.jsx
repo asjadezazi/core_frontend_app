f19e17228df0d6c03159df9c981381f302fe3471
Samad <samadmalikz123@example.com>
2025-02-21 03:29:17 -0800

    add on error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import React from 'react'
 import Loader from './components/common/Loader'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Error from './components/common/Error'
 
 const App = () => {
   return (
+
     <>
-   <Router>
-   <Header/>
-    <Routes>
-      <Route exact path ='/' element={<h1>Home</h1>}/>
-      <Route exact path ='/search' element={<h1>search</h1>}/>
-      <Route exact path ='/about' element={<h1>about</h1>}/>
-      <Route exact path ='/contact' element={<h1>contack</h1>}/>
-      <Route  path ='*' element={<h1>OOP'S bhai aap galat location pr ho</h1>}/>
-
-
-</Routes>
-<h1>this is my footer</h1>
-   </Router>
-   <Error/>
-   </>
-
+      <Router>
+    <Header />
+        <Routes>
+          <Route exact path='/' element={<></>}/>
+          <Route exact path='/home' element={<h1>Home</h1>} />
+          <Route exact path='/search' element={<h1>search</h1>} />
+          <Route exact path='/about' element={<h1>about</h1>} />
+          <Route exact path='/contact' element={<h1>contact</h1>} />
+          <Route  path='*' element={<Error/>} />
+        </Routes>
+      <h1>this is my footer</h1>
+      </Router>
+    </>
   )
 }
 
--- src/components/common/Error.jsx
+++ src/components/common/Error.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import "./error.css"
 const Error = () => {
   return (
-    <div>Error</div>
+    <>
+    <div className='main'>
+    <h1 id='h1'>OOPS!</h1>
+    <h2 id='h2'>404 - PAGE NOT FOUND</h2>
+    <button id='btn'>Go To HomePage</button>
+    </div>
+    </>
   )
 }
 
